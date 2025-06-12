02315f4175a9a3977e66f898023f8b7d93135d27
Dania-f <daniafayyaz919@outlook.com>
2025-06-12 05:09:36 -0400

    completed CartItem funcitonality: updateQuantity, removeItem

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useSelector} from 'react-redux';
 import { useDispatch } from 'react-redux'; // for dispatching Redux actions
 import { addItem } from '../store/CartSlice'; // update the path if needed
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
+    const CartItems = useSelector((state) => state.cart.items);
+    const calculateTotalQuantity =()=>{
+        return CartItems ? CartItems.reduce((total, item)=> totoal + item.quantity,0):0;
+    };
+
     const handleAddToCart = (product) => {
         dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
       
